690b72801793a3c8fffa01028ec3d96008781d59
Magnus Ahlberg <magnus@onboardgames.se>
2016-11-02 17:29:26 -0400

    WIP

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "swog",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 7)
+//         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 32)
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,14 +1,37 @@
-import Kitura
-import KituraNet
-import KituraSys
+// import Kitura
+import Foundation
 
-let router = Router()
+// let router = Router()
+let path = "posts/"
+let dirEnum = FileManager().enumerator(atPath: path)
+var articles = [String: String]()
+let formatter = DateFormatter()
+formatter.dateFormat = "yyyy-M-d"
 
-router.get("/") {
-request, response, next in
-    response.status(HttpStatusCode.OK).send("Hello World from Swift!")
+if dirEnum != nil {
+	while let file = dirEnum!.nextObject() as? String {
+		if file.hasSuffix("md") {
+			print("File: \(file)")
+			let pathComponents = file.components(separatedBy: "/")
+			if pathComponents.count > 2 {
+				let dateString = pathComponents[0]+"-"+pathComponents[1]+"-"+pathComponents[2]
+				let articleDate = formatter.date(from: dateString)
+				print("File: \(file), Date: \(articleDate)")
+			}
+			if let fileContents = try? String(contentsOfFile: path+file, encoding: String.Encoding.utf8) {
+				articles[file] = fileContents
+			}
+		}
+	}
+}
+    
+/* router.get("/") {
+	request, response, next in
+    response.send("temp")
     next()
 }
 
-let server = HttpServer.listen(8090, delegate: router)
-Server.run()
+Kitura.addHTTPServer(onPort: 8090, with: router)
+
+Kitura.run()
+*/
